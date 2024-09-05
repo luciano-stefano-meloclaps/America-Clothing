e4a2ba93e936d5ffa5a91c4caad7865bd6311d87
luciano-meloclaps <meloclapsluciano@gmail.com>
2024-09-05 20:31:15 -0300

    Maqueta Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import NavbarMain from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <NavbarMain />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
