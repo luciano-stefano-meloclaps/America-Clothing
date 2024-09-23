6977d431beb6e3d43c65da1c4abe4dc9ed4ad308
novoluisina <novoluisina@gmail.com>
2024-09-23 10:30:46 -0300

    product-controller

---
+++ Backend/Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.ApplicationDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        private readonly VintageDbContext _context;
+        public ProductRepository(VintageDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Product? GetProductsByName(string name)
+        {
+            return _context.Products.SingleOrDefault(x => x.Name == name);
+        }
+    }
+}
