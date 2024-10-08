58a60b9aea735a595f45b9137d713b0d56bbb6df
luciano-meloclaps <meloclapsluciano@gmail.com>
2024-10-08 00:18:16 -0300

    Componente y estilos AboutUs

--- Frontend/src/components/mainPage/MainPage.jsx
+++ Frontend/src/components/mainPage/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../hero/Hero";
 import ProductList from "../productList/ProductList";
+import AboutUs from "../aboutUs/AboutUs";
 
 const MainPage = () => {
   return (
     <div>
       <Hero />
       <ProductList />
+      <AboutUs />
     </div>
   );
 };
