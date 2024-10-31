9b3160f992450e5e6b6e76d9887209198cfe9a22
CamiloCarabajal <ultrakin3t@gmail.com>
2024-10-31 18:38:54 -0300

    feature/AdminController

--- Backend/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Backend/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb9d241d48f29f7ce15a92b7542f6e95087892f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e96acef0b41818dd7611ba4ba92bcfb3b22da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
